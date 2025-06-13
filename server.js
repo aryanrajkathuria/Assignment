e68b9b83d9ec6ad9411e63f099c75449a2fdae8b
aryanrajkathuria <aryanrajkathuria@gmail.com>
2025-06-13 18:19:01 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,80 @@
+// server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const Product = require('./models/Product');
+
+
+const app = express();
+
+
+
+app.use(bodyParser.json());
+
+// MongoDB connection
+mongoose.connect('mongodb://localhost:27017/store', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error(err));
+
+app.get('/', (req, res) => {
+    res.send('Server is up and running');
+});
+
+  // Create a product
+app.post('/products', async (req, res) => {
+    try {
+        const product = new Product(req.body);
+        await product.save();
+        res.status(201).send(product);
+    } catch (err) {
+        res.status(400).send(err);
+    }
+});
+
+// Get all products
+app.get('/products', async (req, res) => {
+    try {
+        const products = await Product.find();
+        res.send(products);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+// Get single product by ID
+app.get('/products/:id', async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        if (!product) return res.status(404).send('Not found');
+        res.send(product);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+// Update product
+app.put('/products/:id', async (req, res) => {
+    try {
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!product) return res.status(404).send('Not found');
+        res.send(product);
+    } catch (err) {
+        res.status(400).send(err);
+    }
+});
+
+// Delete product
+app.delete('/products/:id', async (req, res) => {
+    try {
+        const product = await Product.findByIdAndDelete(req.params.id);
+        if (!product) return res.status(404).send('Not found');
+        res.send({ message: 'Deleted successfully' });
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+// Start the server
+app.listen(3000, () => console.log('Server running on port 3000'));
